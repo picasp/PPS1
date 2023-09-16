daf064e9e8c2df4dd53e92faad7ad330ac5a7ef3
picasp <abdulrozak@students.amikom.ac.id>
2023-09-16 13:26:38 +0700

    Update Profil

--- app/src/main/java/com/example/p1/MainActivity.java
+++ app/src/main/java/com/example/p1/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.p1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView profileImage;
+    private TextView username, studentInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        profileImage = findViewById(R.id.profileImage);
+        username = findViewById(R.id.username);
+//        studentInfo = findViewById(R.id.studentInfo);
+
+        username.setText("Abdul Rozak Mubarok");
+        studentInfo.setText("Jurusan: Teknik Informatika\nNIM: 21.01.4610");
+        profileImage.setImageResource(R.drawable.default_profile_image);
     }
 }
